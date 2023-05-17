5e13daf585e5c69fd7f3e0a20e60592e5c6e6c69
Kevin <kgutierrez34@.ucol.mx>
2023-05-16 19:42:34 -0600

    Programa

---
+++ Parcial 2/secant.js
@@ -0,0 +1,167 @@
+/** METODO DE BISECCIÓN*/
+
+window.onload = () => {
+
+  resetValues(); 
+
+  //Mostrar el critero de paro elegido
+  let stopMethods = document.getElementById("stopMethods");
+  stopMethods.value = '0';
+  stopMethods.addEventListener('change', function(e){
+    switch(this.value) {
+      case '0':
+        document.getElementById("iteration").value = 0;
+        document.getElementById("iterationMethod").style.display = "none";
+        document.getElementById("criteriaMethod").style.display = "block";
+        break;
+      case '1':
+        document.getElementById("criteria").value = 0;
+        document.getElementById("criteriaMethod").style.display = "none";
+        document.getElementById("iterationMethod").style.display = "block";
+        break;
+      case '2':
+        document.getElementById("iterationMethod").style.display = "none";
+        document.getElementById("criteriaMethod").style.display = "none";
+        break;
+    }
+  });
+
+  //Tomar valores si se presiona el botón de "resultado"
+  document.getElementById('result').addEventListener('click', function(e) {
+    let xL = document.getElementById('xL').value;
+    let xU = document.getElementById('xU').value;
+    let squareVal = document.getElementById('xSqr').value;
+    let linVal = document.getElementById('xLnl').value;
+    let independentVal = document.getElementById('indVal').value;
+    let criteriaOption = document.getElementById('stopMethods').value;
+
+    //Validar que los campos no estén vacíos ni incorrectos
+    values = [xL, xU, squareVal, linVal, independentVal];
+    if (checkValues(values, +criteriaOption) == 4) {
+      let calculator = new Secant();
+      let res = calculator.secant(+xL, +xU, +squareVal, +linVal, +independentVal, +criteriaOption);
+      document.getElementById("root").value = res;
+    } else if (checkValues(values, +criteriaOption) == 3) {
+      document.getElementById('Warningnumber').style.display = "block";
+    } else if (checkValues(values, +criteriaOption) == 1) {
+      document.getElementById('Warningpercentaje').style.display = "block";
+    } else if (checkValues(values, +criteriaOption) == 2) {
+      document.getElementById('Warningiterationin').style.display = "block";
+    } else if (checkValues(values, +criteriaOption) == 5) {
+      document.getElementById('Warningsame').style.display = "block";
+    }
+  });
+
+  //Resetear valores a 0
+  document.getElementById('reset').addEventListener('click', function(e) {
+    resetValues();
+  });
+}
+
+function resetValues() {
+  document.getElementById('xL').value = 0;
+  document.getElementById('xU').value = 0;
+  document.getElementById('xSqr').value = 0;
+  document.getElementById('xLnl').value = 0;
+  document.getElementById('indVal').value = 0;
+  document.getElementById('criteria').value = 0;
+  document.getElementById('iteration').value = 0;
+  document.getElementById('root').value = 0;
+}
+
+function checkValues(values, criteriaOption) {
+  for (let val of values) {
+    if (isNotNumberValid(val)) {
+      return 3;
+    }
+  }
+  //check values for stopping iteration
+  if (criteriaOption == 0){
+    let percentage = document.getElementById('criteria').value;
+    if (isNotNumberValid(percentage)) {
+      return 3;
+    } else if (percentage <= 0) {
+      return 1; //if percentage is less than 0 or not a number
+    }
+  }
+  if(criteriaOption == 1) {
+    let iterations = document.getElementById('iteration').value;
+    if (isNotNumberValid(iterations)) {
+      return 3;
+    } else if (iterations <= 0) {
+      return 1; //if iterations is less than 0 or not a number
+    } else if (!(iterations%1==0)){
+      return 2; //if iterations is not an integer
+    }
+  }
+  if (values[0] == values[1]) {
+    return 5; //if xL equal to xU
+  }
+  return 4; //only if all values are valid
+}
+
+function isNotNumberValid(val) {
+  if (val == "0") {
+    return false;
+  }
+  return (isNaN(val) && isNaN(parseFloat(val))) || val.trim() == "" || !val;
+}
+
+//Clase con el método para calcular el resultado  (Oiga compañero Aldo que es esto xd) queso
+class Secant {
+  secant(xL, xU, squareVal, linVal, independentVal, criteriaOption) {
+    let fXL = this.functionX(xL,squareVal,linVal,independentVal);
+    let fXU = this.functionX(xU,squareVal,linVal,independentVal);
+
+    //Calcular primera X
+    let xR = (xU - ((fXU*(xL-xU)) / (fXL - fXU)));
+    let funcXR = this.functionX(xR,squareVal,linVal,independentVal);
+
+    let criteria = 0;
+
+    //Iniciar iteraciones
+    do {
+      console.log(xL, xU, xR);
+      xL = xU;
+      xU = xR;
+      fXL = this.functionX(xL,squareVal,linVal,independentVal);
+      fXU = this.functionX(xU,squareVal,linVal,independentVal);
+      xR = (xU - ((fXU*(xL-xU)) / (fXL - fXU)));
+      funcXR = this.functionX(xR,squareVal,linVal,independentVal);
+      
+      //Utilizar un metodo para evaluar si parar o no
+      if(criteriaOption == 0 || criteriaOption == 2) {
+        criteria = this.relativeError(xR, xU);
+      } else if(criteriaOption == 1) {
+        criteria++;
+      }
+
+    } while(this.evaluateStop(criteria, criteriaOption));
+      
+    return xR;
+  }
+
+  evaluateStop(criteria, option) {
+    switch(option) {
+      case 0:
+        return criteria > +(document.getElementById('criteria').value);
+        break;
+      case 1:
+        return criteria < (+(document.getElementById('iteration').value) - 1);
+        break;
+      case 2:
+        return criteria >= 0.001;
+        break;
+    }
+  }
+
+  functionX(x, squareVal, linVal, independentVal) {
+    return (squareVal*(x*x))+(linVal*x)+independentVal;
+  }
+
+  relativeError(newVal, previousVal) {
+    let error = (((newVal - previousVal)/newVal)*100);
+    return (error >= 0) ? error : (error * -1);
+  }
+
+}
